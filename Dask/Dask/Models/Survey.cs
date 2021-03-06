74dd0ff5b4188370fd9f7f168a3b3cacc44802ca
denyo1337 <dannio1337@gmail.com>
2021-03-06 18:55:40 +0100

    Added Author prop

--- Dask/Dask/Models/Survey.cs
+++ Dask/Dask/Models/Survey.cs
@@ -11,6 +11,7 @@ namespace Dask.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Author { get; set; }
         public string Description { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
         public string ApplicationUserId { get; set; }
