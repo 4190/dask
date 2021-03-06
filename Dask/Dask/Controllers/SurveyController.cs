8bf673750dc87dd46cc668951adcd4884ebda1ce
denyo1337 <dannio1337@gmail.com>
2021-03-06 18:53:48 +0100

    Created SurverController

---
+++ Dask/Dask/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dask.Services;
+
+
+namespace Dask.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private readonly IManageSurveysService _surveysService;
+
+        public SurveyController(IManageSurveysService surveyService)
+        {
+            this._surveysService = surveyService;
+        }
+        public IActionResult Index()
+        {
+            var item = _surveysService.GetAllSurveys();
+
+
+            return View();
+        }
+    }
+}
