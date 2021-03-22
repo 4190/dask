46e3c6f101e8c610658453dce8aabcace39897e2
denyo1337 <dannio1337@gmail.com>
2021-03-22 22:41:09 +0100

    Added QuestionRepository and service, yet no implementation

---
+++ Dask/Dask/Data/EfCore/EfCoreQuestionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dask.Data.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dask.Models;
+using Dask.Data;
+
+namespace Dask.Data.EfCore
+{
+    public class EfCoreQuestionRepository : EfCoreRepository<Question, ApplicationDbContext>
+    {
+        public EfCoreQuestionRepository(ApplicationDbContext context):base(context)
+        {
+
+        }
+    }
+}
